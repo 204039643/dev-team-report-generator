416229b7af48d480a2f149af0ce54e7e114454f1
Justin Sykes <atlsykes1@att.net>
2020-11-10 17:26:03 -0500

    updated 3 sub-classes to inherit name, id, email, from main class Employee; got all unit tests to pass

--- app.js
+++ app.js
@@ -32,3 +32,11 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+const promptUser = () =>
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the project title?'
+    }]);
